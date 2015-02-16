df9de680ef297a40971a510676db0b16f221d6db
Igor Bobko <bobko@teamidea.ru>
2015-02-16 16:28:24 +0300

    progress

---
+++ src/ru/todo100/social/vk/strategy/Operations.java
@@ -0,0 +1,7 @@
+package ru.todo100.social.vk.strategy;
+
+/**
+ * Created by igor on 08.02.15.
+ */
+public class Operations {
+}
---
+++ src/sample/UserGroupsController.java
@@ -0,0 +1,7 @@
+package sample;
+
+/**
+ * Created by igor on 08.02.15.
+ */
+public class UserGroupsController {
+}
