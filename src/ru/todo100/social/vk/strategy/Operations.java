63d98bc5ea5b34b8c253b4d0f7208f1863b0a55c
Igor Bobko <bobko@teamidea.ru>
2015-02-16 16:29:40 +0300

    all

--- src/ru/todo100/social/vk/strategy/Operations.java
+++ src/ru/todo100/social/vk/strategy/Operations.java
@@ -4,4 +4,10 @@ package ru.todo100.social.vk.strategy;
  * Created by igor on 08.02.15.
  */
 public class Operations {
+    protected String accessToken;
+
+    public Operations(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
 }
